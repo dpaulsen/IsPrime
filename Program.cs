2db7e5963fd21c2a4d332768b12c96dbbe07c0a1
Daniel Paulsen <daniel.t.paulsen@gmail.com>
2015-10-09 17:07:53 -0400

    creating repository
    Has too much stuff.
    
    program just prints out all inputs given via commandline.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for ( int i =0; i < args.Count(); ++i)
+            {
+                // there must be a better way/....
+                //Console.Write("i = %d ", i, " | args[i] = ", args[i]);
+                Console.Write("i = ");
+                Console.Write(i);
+                Console.Write(" | args[i] = ");
+                Console.Write(args[i]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
