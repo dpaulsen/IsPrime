d6fb57f48d7023902ff036a6d36f1340b2563df7
Daniel Paulsen <daniel.t.paulsen@gmail.com>
2015-10-13 08:16:01 -0400

    added file.
    moved file to directory

---
+++ PrimeNumber.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+public class PrimeNumber
+
+{
+    private int number;
+    private bool isInteger;  //should this be BOOLEAN class?
+
+    static private int firstPrime = 2; 
+
+    public PrimeNumber()
+    {
+        isInteger = false;
+    }
+
+    public PrimeNumber(int input)
+    {
+        number = input;
+        isInteger = true;
+    }
+
+    public PrimeNumber(String input)
+    {
+       isInteger = int.TryParse(input, number);
+    }
+
+    public bool IsInteger()
+    {
+        return isInteger;
+    }
+
+    public bool isPrime()
+    {
+        // use a switch statement ???
+
+        // could do <= 1 but... each is a specialcase
+
+        if (number < 0) return false;
+        if (number == 0) return false;
+        if (number == 1) return false;  
+
+        for(int i = firstPrime; i < number; ++i)
+        {
+            if (number % i == 0) goto blackmagic; 
+        }
+
+        return true;
+
+    blackmagic:
+        return false;
+
+    }
+
+    public String display()
+    {
+        StringBuilder output;
+
+        output = number.ToString + " is "; 
+
+        // yeah you are probably not going to like this...
+        if(!IsInteger()) { output = output + " not an integer"; } else output = output + isPrime() ? " Prime" : "not prime";
+
+        return output.ToString();
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace PrimeNumber
     {
         class Program
         {
-            public class PrimeNumber numberToCheck;
+           // public class PrimeNumber numberToCheck;
 
             /* a prime number only has factors 1 and itself 
                This program checks if a number entered to the commandline is a prime number or not
@@ -18,7 +18,7 @@ namespace PrimeNumber
 
             static void Main(string[] args)
             {
-                numberToCheck = PrimeNumber(args[0]);
+              //  numberToCheck = PrimeNumber(args[0]);
                 /*  
                 // yes this is an ugly way to remove this stuff....
                 for ( int i =0; i < args.Count(); ++i)
