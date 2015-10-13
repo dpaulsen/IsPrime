d30361fff34b5e0535609885ce5e7fdb338a1fae
Daniel Paulsen <daniel.t.paulsen@gmail.com>
2015-10-13 10:29:25 -0400

    started unit test
    namespace issue, will look at later.

---
+++ PrimeNumberTests/TestPrimes.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimeNumberTests
+{
+    [TestClass]
+    public class TestPrimes
+    {
+      //  private PrimeNumber prime;  //namespace...
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
